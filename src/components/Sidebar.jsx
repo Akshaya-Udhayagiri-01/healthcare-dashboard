2d804142e19a0dbdcc85a70b4d925310e29cb378
Akshaya Udhayagiri <udhayagiriakshaya@gmail.com>
2025-05-27 19:46:29 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 export default function Sidebar({ className = '' }) {
   return (
-    <aside className="sidebar">
+    <aside className={className}>
       <div className="sidebar-logo">Healthcare.</div>
 
       <h2>General</h2>
@@ -10,12 +10,15 @@ export default function Sidebar({ className = '' }) {
         <li>Calendar</li>
         <li>Appointments</li>
         <li>Statistics</li>
-      </ul> <br />
+      </ul>
+      <br />
+
       <h2>Tools</h2>
       <ul>
         <li>Tests</li>
         <li>Chat</li>
-        <li>Support</li> <br /><br /><br /><br /><br /><br /><br />
+        <li>Support</li>
+        <br /><br /><br /><br /><br /><br /><br />
         <li><h2>Settings</h2></li>
       </ul>
     </aside>
